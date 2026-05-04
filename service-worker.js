32173cd812d0121c4c88af6782755c7459e21feb
le0tec <leoferreiran7@gmail.com>
2026-05-04 16:44:30 -0300

    Refactor service worker caching logic
    Updated cache name and paths in service worker.

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
-        "/",
+        "./",
         "/index.html",
         "/style.css",
         "/script.js"
